06e6e41b5b90daa6a21cedc22754cbbeb4926824
mycodeareana <mitty.frost@gmail.com>
2019-01-12 19:14:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 angular.module('myApp', [])
 .controller('myBlogPost', function($scope, $http) {
-    $http.get('https://script.google.com/macros/s/AKfycbyAZekrWJdPuGu-7zPSmSLCFgY0f4s-wHBtvyYNvJK-NU1GTYcl/exec?action=get&blogpostid=NdAr2eP1yi8&v=' + Date.now()).
+    $http.get('https://script.google.com/macros/s/AKfycbyAZekrWJdPuGu-7zPSmSLCFgY0f4s-wHBtvyYNvJK-NU1GTYcl/exec?action=get&blogpostid=NdAr2eP1yi8&callback=?&v=' + Date.now()).
         then(function(response) {
             $scope.blogpost = response.data;
         });
