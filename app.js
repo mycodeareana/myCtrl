26e2589d2204150182bb50fe97b6d4cdcd192ad5
mycodeareana <mitty.frost@gmail.com>
2019-01-13 02:09:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 angular.module('myApp', [])
-.controller('myBlogPost', function($scope, $http) {
-    $http.get('https://script.google.com/macros/s/AKfycbyAZekrWJdPuGu-7zPSmSLCFgY0f4s-wHBtvyYNvJK-NU1GTYcl/exec?action=get&blogpostid=NdAr2eP1yi8&callback=?&v=' + Date.now()).
-        then(function(response) {
-            $scope.blogpost = response.data;
-        });
+.config(['$sceDelegateProvider', function($sceDelegateProvider) {
+  // We must whitelist the JSONP endpoint that we are using to show that we trust it
+  $sceDelegateProvider.resourceUrlWhitelist([
+    'self',
+    'https://script.google.com/**',
+    'https://angularjs.org/**'
+  ]);
+}])
+.controller('myBlogPost', function ($scope, $http) {
+	$scope.test = "If you can see this the mainController works"
+	
+    $http.jsonp('https://script.google.com/macros/s/AKfycbyAZekrWJdPuGu-7zPSmSLCFgY0f4s-wHBtvyYNvJK-NU1GTYcl/exec?action=get&blogpostid=NdAr2eP1yi8&v=' + Date.now(), {jsonpCallbackParam: 'callback'}).
+		then(function (data) {
+			console.log(data.data.blogposts);
+			$scope.blogposts = data.data.blogposts;
+			$scope.Title = data.data.blogposts[0].Title;
+		});
+		
+		
 });
