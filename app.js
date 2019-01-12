7bc4a3eef56f9157ac14fbccf1e406863aa84afd
mycodeareana <mitty.frost@gmail.com>
2019-01-13 02:12:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ angular.module('myApp', [])
   $sceDelegateProvider.resourceUrlWhitelist([
     'self',
     'https://script.google.com/**',
-    'https://angularjs.org/**'
+    'https://angularjs.org/**',
+	  'https://cdn.jsdelivr.net/gh/mycodeareana/myCtrl/**'
   ]);
 }])
 .controller('myBlogPost', function ($scope, $http) {
